6ae6429f6c938bf01251b3c466d13963108fb4c2
neel907 <sufolneel@gmail.com>
2025-03-18 00:18:04 +0600

    Add files via upload

--- SQL5.sql
+++ SQL5.sql
@@ -1,33 +1,105 @@
-CREATE TABLE IF NOT EXISTS DEPARTMENT (
-  EMPLOYEE_ID TEXT,
-  NAME TEXT,
-  DEPARTMENT_ID TEXT,
-  MANAGER_ID TEXT,
-  SALARY REAL
+CREATE TABLE IF NOT EXISTS Salesman (
+Salesman_id TEXT PRIMARY KEY,
+name TEXT,
+city TEXT,
+Comission REAL
 );
 
-INSERT INTO DEPARTMENT (EMPLOYEE_ID, NAME, DEPARTMENT_ID, MANAGER_ID, SALARY) VALUES
-  ('100', 'STEVEN KING', '90', '100', 24000),
-  ('101', 'NEENA KOCCHAR', '90', '100', 17000),
-  ('102', 'LEX DEHAAN', '90', '102', 9000),
-  ('103', 'BRUCE LEE', '60', '103', 4800),
-  ('104', 'DIANA WILLS', '60', '103', 25000),
-  ('105', 'VALLI PATOR', '50', '100', 4200),
-  ('1973', 'LUV HAMI', '60', '102', 5000),
-  ('106', 'DAVID AUSTIN', '90', '100', 6000);
+
+INSERT INTO Salesman (Salesman_id, name, city, Comission) VALUES
+('5001', 'James Hoog', 'New York', 0.15),
+('5002', 'Nail Knite', 'Paris', 0.13),
+('5005', 'Pit Alex', 'London', 0.11),
+('5006', 'Mc Lyon', 'Paris', 0.14),
+('5007', 'Paul Adam', 'Rome', 0.13),
+('5003', 'Lauson Hen', 'San Jose', 0.12);
 
 
-SELECT department_id AS "Department Code", 
-COUNT(*) AS "No of Employees" 
-FROM DEPARTMENT
-GROUP BY department_id; 
 
-SELECT department_id, SUM(salary) 
-FROM DEPARTMENT 
-GROUP BY department_id;
+CREATE TABLE IF NOT EXISTS Customer (
+customer_id TEXT PRIMARY KEY,
+cust_name TEXT ,
+city TEXT,
+grade INTEGER,
+Salesman_id TEXT,
+FOREIGN KEY (Salesman_id) REFERENCES Salesman(Salesman_id)
+);
 
-SELECT department_id AS "Department Code", 
-COUNT(*) AS "No of Employees", 
-SUM(salary) AS "Total Salary" 
-FROM DEPARTMENT 
-GROUP BY department_id; 
+
+
+INSERT INTO Customer (customer_id, cust_name, city, grade, Salesman_id) VALUES
+('3002', 'nick rimando', 'new york', 100, '5001'),
+('3007', 'brad davis', 'new york', 200, '5001'),
+('3005', 'graham zusi', 'california', 200, '5002'),
+('3008', 'julian green', 'london', 300, '5002')
+('3004', 'fabian johnson', 'paris', 300, '5006'),
+('3009', 'geoff cameron', 'berlin', 100, '5003'),
+('3003', 'jozy altidor', 'moscow', 200, '5007'),
+('3001', 'brad guzan', 'london', NULL, '5005');
+
+CREATE TABLE IF NOT EXISTS Orders (
+
+ord_no TEXT PRIMARY KEY,
+
+purch_amt REAL,
+
+ord_date TEXT,
+
+customer_id TEXT,
+
+Salesman_id TEXT,
+
+FOREIGN KEY (customer_id) REFERENCES Customer(customer_id),
+
+FOREIGN KEY (Salesman_id) REFERENCES Salesman(Salesman_id)
+
+);
+
+INSERT INTO Orders (ord_no, purch_amt, ord_date, customer_id, Salesman_id) VALUES
+
+('70001', 150.5, '2012-10-05', '3005', '5002'),
+
+('70009', 270.65, '2012-09-10', '3001', '5001'),
+
+('70002', 65.26, '2012-10-05', '3002', '5003'),
+
+('70004', 110.5, '2012-08-17', '3009', '5007'),
+
+('70007', 948.5, '2012-09-10', '3005', '5005'),
+
+('70005', 2400.6, '2012-07-27', '3007', '5006');
+
+
+
+
+SELECT Customer.cust_name, Salesman.name
+FROM Customer 
+JOIN Salesman ON Customer.Salesman_id = Salesman.Salesman_id;
+
+SELECT Orders.ord_no, Cutomer.cust_name, Orders.customer_id, Orders.Salesman_id
+FROM Orders
+JOIN Customers ON Orders.customer_id = Customer.customer_id
+JOIN Salesman ON Orders.salesman_id = Salesman.salesman_id_id
+WHERE Customer.city <> Salesman.city;
+
+SELECT Orders.ord_no, Customer.cust_name
+FROM Orders
+JOIN Customer ON Orders.Customer_id = Customers.customers_id;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
